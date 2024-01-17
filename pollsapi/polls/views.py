a8bd04ccd027065ef1d5b3c785ae24139f431c00
Brij123987 <134289082+Brij123987@users.noreply.github.com>
2024-01-17 12:36:21 +0530

    commit:7

--- pollsapi/polls/views.py
+++ pollsapi/polls/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from django.http import JsonResponse
+from polls.models import Polls
 
 # Create your views here.
 
 def poll_lists(request):
-    pass
+    MAX_OBJECTS = 20
+    polls = Polls.Objects.all() [:MAX_OBJECTS]
+    data = {'results':list(polls.values("question","created_by__username","pub_date"))}
+    return JsonResponse(data)
+
 
 def poll_details(request, pk):
-    pass
+    poll = get_list_or_404(Polls, pk=pk)
+    data = {
+        "results":{
+            "question":poll.question,
+            "created_by":poll.created_by.username,
+            "pub_date":poll.pub_date
+
+        }
+    }
+    return JsonResponse(data)
 
