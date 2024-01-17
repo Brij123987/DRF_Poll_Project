d7533fedc30f2021d967879875dae2197de6f04d
Brij123987 <134289082+Brij123987@users.noreply.github.com>
2024-01-17 12:25:46 +0530

    commit:6

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
+from polls import views
+
+# URLS ------------------------------------
 
 urlpatterns = [
-    
+    path('polls/',views.poll_lists, name='poll_lists'),
+    path('polls/<int:pk>/',views.poll_details, name='poll_details'),
 ]
--- pollsapi/polls/views.py
+++ pollsapi/polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def poll_lists(request):
+    pass
+
+def poll_details(request, pk):
+    pass
+
